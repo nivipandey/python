b2defaf64e9f4a3d0f4ce6c5fbf7d81f1b6929c1
nivipandey <55634422+nivipandey@users.noreply.github.com>
2020-06-16 00:40:03 +0530

    Add files via upload

---
+++ set/set1.py
@@ -0,0 +1,14 @@
+s1={1,2,3,4,5,6,7,8,9,20}
+print(s1)
+str1="sudipta"
+s2=set(str1)
+print(s2)
+s2.update([10,20])
+print(s2)
+s2.remove(10)
+print(s2)
+x=100
+y=200
+print(x and y)
+print(x or y)
+print(not x)
---
+++ set/set2.py
@@ -0,0 +1,10 @@
+x=10
+y=20
+print("sum of ",x, " and ",y," is ",x+y)
+
+print("n1 = {0} ".format(x))
+x=eval(input("enter the expression"))
+print("result %d " %x)
+
+l1=eval(input("enter the list"))
+print("sum",sum(l1))
