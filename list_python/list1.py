493fae2618760657ed7cc1657a95891860bbdbde
nivipandey <55634422+nivipandey@users.noreply.github.com>
2020-06-16 00:39:02 +0530

    Add files via upload

---
+++ list_python/list1.py
@@ -0,0 +1,12 @@
+l=[]
+l=[1,2,3,4]
+l=[1,2,"ertt",45]
+l=[1,2,3,34.56,"tiffc",[1,"fid","sudi",56.7]]
+print(l)
+print(l[5][1])
+print(l[-1])
+print(l[-2])
+print(l[-1][1])
+print(l[5][:1])
+#print(l[1:[5][2]])
+
